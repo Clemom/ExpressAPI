e53206b26ac323f84d908aa2f2aa8d2847a69a3d
Clemom <clement33.moreaupro@gmail.com>
2026-03-27 04:19:12 +0100

    add JWT Token + Cookie (no refresh token yet)

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from "express";
 import { registerSchema, loginSchema } from "../schemas/user.schema";
 import { userService } from "../services/user.service";
+import { generateAccessToken } from "../lib/jwt";
 
+export const me = async (req: Request, res: Response) => {
+  const userId = req.user!.id;
+
+  const user = await userService.getUserById(userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const { password, ...safeUser } = user;
+
+  res.json(safeUser);
+};
 
 export const register = async (req: Request, res: Response) => {
   const data = registerSchema.parse(req.body);
@@ -17,5 +31,20 @@ export const login = async (req: Request, res: Response) => {
 
   const user = await userService.loginUser(data);
 
-  res.status(200).json(user);
-}
+  const token = generateAccessToken({
+    id: user.id,
+    email: user.email,
+  });
+
+  res.cookie("accessToken", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    path: "/",
+  });
+
+  res.status(200).json({
+    id: user.id,
+    email: user.email,
+  });
+};
